90376805b66b41ed605d771a91747cef86ffe8eb
tindrq <blomgren.tindra@gmail.com>
2024-05-20 18:27:42 +0200

    iteration1

--- backend/test/unit/test_solution.py
+++ backend/test/unit/test_solution.py
@@ -1,6 +1,20 @@
 import pytest
+import unittest.mock as mock
 
-# add your test case implementation here
+from src.controllers.recipecontroller import RecipeController
+from src.static.recipes import from_string
+@pytest.fixture
+def sut():
+    mockeList = mock.MagicMock()
+    mockeList.get_readiness_of_recipes.return_value = ['Banana Bread']
+    mockedsut = RecipeController(dao=mockeList)
+    return mockedsut
+
+
 @pytest.mark.unit
-def test():
-    pass
+@pytest.mark.parametrize('take, expected' [(True, 'Banana Bread', )])
+def optimal(sut, take, expected):
+    diet = from_string('vegetarian')
+    validationresult = sut.get_recipe(diet=diet, take_best=take)
+    print(validationresult)
+    assert validationresult == expected 
